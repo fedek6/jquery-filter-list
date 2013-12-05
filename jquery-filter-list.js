a375cc8e1d4c8f99e3bfdb1168cf7f679c44f758
Konrad Fedorczyk <fedek6@gmail.com>
2013-12-05 16:17:31 +0100

    first version

--- jquery-filter-list.js
+++ jquery-filter-list.js
@@ -19,13 +19,17 @@
         // variables declaration
         var $input = $(options.inputSelector),
             $ul = this;
+            
+       	if(options.zebra) {
+       		$ul.find('li').filter(':odd').addClass('even');
+       	}
 
         // on default attach
         $input.keyup(function (e) {
             var txt = $(this).val();
 
             $ul.each(function () {
-                $(this).find('li').hideByContent(txt, options);
+                $(this).find('li').not('.no-results').hideByContent(txt, options);
             });
         });
 
@@ -34,21 +38,55 @@
     }
     // hide element if it has text
     $.fn.hideByContent = function(txt, options) {
-        return this.each(function() { 
+    	var count = this.length,
+    		i = 0
+    		visibleIndex = 0;
+        
+        this.each(function() { 
             var content = $(this).text();
             
-            window.console.log(options.caseSensitive);
             if(!options.caseSensitive) {
                 content = content.toLowerCase();
                 txt = txt.toLowerCase();
             }
             
+            if(options.zebra) {
+            	$(this).removeClass('even');
+            }
+            
             if (content.indexOf(txt) === -1) { 
                 $(this).stop().hide(options.duration);
+                ++i;
             } else {
+            	if(options.zebra && ++visibleIndex%2 == 0) {
+            		$(this).addClass('even');
+            	}
                 $(this).stop().show(options.duration);
             }                        
         });
+        
+        // if all elements were removed
+		if( count === i && !this.data().hadNoContent ) {
+        	// create no results indicator
+        	if(!this.data().indicator) {
+        		var noResults = $('<li></li>').addClass('no-results').text(options.noResultsText).appendTo(this.parent());
+        		this.data('indicator', noResults);
+        	}
+        	else {
+        		var noResults = this.data().indicator;
+        	}
+        	
+        	noResults.show();
+        	
+        	this.data('hadNoContent', true);
+        } 
+        // else not all elements were removed
+        else if(count > i && this.data().hadNoContent ) {
+        	this.data().indicator.hide();
+        	this.data('hadNoContent', false);
+        }
+        
+        return this;
     }
 
     // default options
@@ -56,10 +94,7 @@
         method: 'attach',
         duration: 100,
         caseSensitive: true,
+        noResultsText: 'There are no results.',
+        zebra: true
     }
 }(jQuery));
-
-$('ul#dynamic-search-list').filterList({
-    inputSelector: '#filter-input',
-    caseSensitive: false,
-});
